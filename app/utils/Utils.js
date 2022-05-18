0a076f73c5e51c8da784eaae50b4fb8066825814
Xtrendence <xtrendence@gmail.com>
2022-05-18 16:17:52 +0100

    Added function for parsing OTPAuth URIs.

--- app/utils/Utils.js
+++ app/utils/Utils.js
@@ -17,3 +17,36 @@ export function wait(duration) {
 		}, duration);
 	});
 }
+
+export function parseURI(uri) {
+	if(typeof uri !== "string" || uri.length < 7) {
+		return null;
+	}
+
+	let parts = /otpauth:\/\/([A-Za-z]+)\/([^?]+)\??(.*)?/i.exec(uri);
+
+	if(!parts || parts.length < 3) { 
+		return null; 
+	}
+
+	let [fullUri, type, fullLabel] = parts;
+
+	if(!type || !fullLabel) { 
+		return null; 
+	}
+
+	let account = decodeURIComponent(fullLabel);
+
+	let labelParts = account.split(/: ?/);
+
+	let label = labelParts && labelParts.length === 2 ? { issuer:labelParts[0], account:labelParts[1] } : { issuer:"", account:account };
+
+	let queryString = parts[3] ? new URLSearchParams(parts[3]) : [];
+
+	let query = [...queryString].reduce((info, [key, value]) => {
+		info[key] = value;
+		return info;
+	}, {});
+
+	return { type:type.toLowerCase(), label, query };
+}
