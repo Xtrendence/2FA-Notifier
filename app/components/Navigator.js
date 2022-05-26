210e48a47cf35ff2285a7bb434ff401fd84fbd78
Xtrendence <xtrendence@gmail.com>
2022-05-27 00:26:09 +0100

    Moved Toast configuration.

--- app/components/Navigator.js
+++ app/components/Navigator.js
@@ -1,46 +1,14 @@
 import React from "react";
 import { NavigationContainer, DarkTheme } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { CardStyleInterpolators } from "@react-navigation/stack";
-import Toast, { BaseToast } from "react-native-toast-message";
+import Toast from "react-native-toast-message";
 import Accounts from "../screens/Accounts";
 import ViewAccount from "../screens/ViewAccount";
 import Settings from "../screens/Settings";
 import Scan from "../screens/Scan";
-import { Colors, GlobalStyle } from "../styles/Global";
+import toastConfig from "../utils/ToastConfig";
 
 const Stack = createNativeStackNavigator();
-
-const toastConfig = {
-	success: (props) => (
-		<BaseToast
-			{...props}
-			style={{ 
-				borderLeftColor: Colors.accentFirst,
-				borderTopColor: Colors.mainFirst,
-				borderTopWidth: 2,
-				borderBottomColor: Colors.mainFirst,
-				borderBottomWidth: 2,
-				borderRightColor: Colors.accentFirst,
-				borderRightWidth: 5,
-			}}
-			contentContainerStyle={{ 
-				paddingHorizontal: 15,
-				backgroundColor: Colors.mainSecond,
-				borderWidth: 0,
-			}}
-			text1Style={{
-				fontSize: 16,
-				fontWeight: "bold",
-				color: Colors.mainContrast
-			}}
-			text2Style={{
-				fontSize: 14,
-				color: Colors.mainContrast
-			}}
-		/>
-	),
-}
 
 export default function Navigator() {
 	return (
