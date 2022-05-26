3c5d3571a23ddcbfcb37a11a7dbd07e70a7a472c
Xtrendence <xtrendence@gmail.com>
2022-05-26 18:13:49 +0100

    Improved "Page" component.

--- app/components/common/Page.js
+++ app/components/common/Page.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { StyleSheet, View, Text } from "react-native";
-import { statusBarHeight } from "./NavigationBar";
+import { screenWidth, statusBarHeight } from "../../utils/Measurements";
 import LinearGradient from "react-native-linear-gradient";
 import { empty } from "../../utils/Utils";
 import Icon from "react-native-vector-icons/FontAwesome5";
@@ -10,21 +10,33 @@ import TouchableScale from "./TouchableScale";
 export default function Page(props) {
 	let gradient = !empty(props?.gradient) ? props.gradient : 0;
 	let angle = !empty(props?.angle) ? props.angle : 45;
+	let noBack = !empty(props?.noBack) ? props?.noBack : false;
+	let noIcon = !empty(props?.noIcon) ? props?.noIcon : false;
+	let icon = !empty(props?.icon) ? props?.icon : "cog";
+	let onPressBack = !empty(props?.onPressBack) ? props?.onPressBack : () => {};
+	let onPressIcon = !empty(props?.onPressIcon) ? props?.onPressIcon : () => {};
 
 	return (
 		<View style={styles.page}>
 			{ !empty(props?.title) &&
 				<View style={styles.top}>
 					<View style={styles.left}>
+						{ !noBack &&
+							<TouchableScale style={styles.back} onPress={onPressBack}>
+								<Icon name="chevron-left" size={32} color={Colors.mainContrast}/>
+							</TouchableScale>
+						}
 						<LinearGradient style={styles.titleCard} colors={Gradients[gradient]} angle={angle} useAngle={true}>
-							<Text style={styles.title}>{props.title}</Text>
+							<Text style={styles.title} ellipsizeMode="tail" numberOfLines={1}>{props.title}</Text>
 						</LinearGradient>
 					</View>
-					<View style={styles.right}>
-						<TouchableScale style={styles.settings}>
-							<Icon name="cog" size={32} color={Colors.mainContrast}/>
-						</TouchableScale>
-					</View>
+					{ !noIcon &&
+						<View style={styles.right}>
+							<TouchableScale style={styles.settings} onPress={onPressIcon}>
+								<Icon name={icon} size={32} color={Colors.mainContrast}/>
+							</TouchableScale>
+						</View>
+					}
 				</View>
 			}
 			{props.children}
@@ -46,10 +58,13 @@ const styles = StyleSheet.create({
 		borderBottomWidth: 2,
 		borderColor: Colors.mainThird
 	},
+	left: {
+		flexDirection: "row"
+	},
 	right: {
 		flexGrow: 1,
 		alignItems: "flex-end",
-		paddingRight: 20,
+		paddingRight: 10,
 	},
 	titleCard: {
 		height: 50,
@@ -67,10 +82,18 @@ const styles = StyleSheet.create({
 	title: {
 		color: Colors.accentContrast,
 		fontSize: GlobalStyle.fontLarge,
-		fontWeight: "bold"
+		fontWeight: "bold",
+		maxWidth: screenWidth - 200,
+	},
+	back: {
+		justifyContent: "center",
+		height: 50,
+		paddingRight: 20,
 	},
 	settings: {
 		justifyContent: "center",
-		height: 50
+		height: 50,
+		paddingRight: 10,
+		paddingLeft: 10,
 	}
 });
