a51649989ee317f7c1f1dc3889273282ac725811
Xtrendence <xtrendence@gmail.com>
2022-06-05 17:52:39 +0100

    Added helper functions for encryption/decryption.

--- app/utils/Utils.js
+++ app/utils/Utils.js
@@ -1,5 +1,6 @@
 import totp from "totp-generator";
 import * as OTPAuth from "url-otpauth";
+import Crypto from "./Crypto";
 
 export function empty(value) {
 	if(typeof value === "object" && value !== null && Object.keys(value).length === 0) {
@@ -33,4 +34,35 @@ export function wait(duration) {
 
 export function parseURI(uri) {
 	return OTPAuth.parse(uri);
+}
+
+export function encryptObjectValues(password, object) {
+	let encrypted = {};
+	let keys = Object.keys(object);
+
+	keys.map(key => {
+		let value = object[key].toString();
+		let ciphertext = Crypto.encryptAES(value, password);
+		encrypted[key] = ciphertext;
+	});
+
+	return encrypted;
+}
+
+export function decryptObjectValues(password, object) {
+	let decrypted = {};
+	let keys = Object.keys(object);
+
+	keys.map(key => {
+		let value = object[key];
+
+		try {
+			let plaintext = Crypto.decryptAES(value, password);
+			decrypted[key] = plaintext;
+		} catch(error) {
+			decrypted[key] = value;
+		}
+	});
+
+	return decrypted;
 }
